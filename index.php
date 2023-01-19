ea3ba3f7beca2e7673b7533e1f5e2e1c796213f2
Kivi Amarakoon <89955961+kivi98@users.noreply.github.com>
2023-01-20 02:02:35 +0530

    added the index page

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+    include "Includes/header.php";
+    include "Includes/footer.php"; 
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style><?php include "Css/style.css" ?></style> 
+</head>
+<body>
+    <div class="landingMain">
+        <div class="landingLeft">
+            <img class="landingPagePhoto" src="Assets\images\common\landing-page-photo.jpg" alt="landing-page-photo">
+        </div>
+        <div class="landingRight">
+            <div class="rightContent">
+                <h1>Welcome to <br> MOM CARE</h1>
+            </div>
+            <img class="logoLarge" src="Assets/images/common/logoLarge.png" alt="Logo">
+            <div>
+                <button class="btnMain" >Register</button>
+                <button class="btnMain" >Sign in</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
